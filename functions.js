c925d250eab06671169a26172d10cb5023161f1f
jiashunming <jiashunming@outlook.com>
2018-02-15 18:08:40 +0800

    update

--- functions.js
+++ functions.js
@@ -15,11 +15,6 @@ $(function () {
     gardenCtx = gardenCanvas.getContext("2d");
     gardenCtx.globalCompositeOperation = "lighter";
     garden = new Garden(gardenCtx, gardenCanvas);
-	
-	$("#content").css("width", $loveHeart.width() + $("#code").width());
-	$("#content").css("height", Math.max($loveHeart.height(), $("#code").height()));
-	$("#content").css("margin-top", Math.max(($window.height() - $("#content").height()) / 2-50, 10));
-	$("#content").css("margin-left", Math.max(($window.width() - $("#content").width()) / 2, 10));
 
     // renderLoop
     setInterval(function () {
@@ -59,7 +54,7 @@ function startHeartAnimation() {
 		}
 		if (draw) {
 			heart.push(bloom);
-			garden.createRandomBloom(bloom[0], bloom[1]);
+			garden.createRandomBloom && garden.createRandomBloom(bloom[0], bloom[1]);
 		}
 		if (angle >= 0) {
 			clearInterval(animationTimer);
@@ -69,28 +64,6 @@ function startHeartAnimation() {
 		}
 	}, interval);
 }
-
-(function($) {
-	$.fn.typewriter = function() {
-		this.each(function() {
-			var $ele = $(this), str = $ele.html(), progress = 0;
-			$ele.html('');
-			var timer = setInterval(function() {
-				var current = str.substr(progress, 1);
-				if (current == '<') {
-					progress = str.indexOf('>', progress) + 1;
-				} else {
-					progress++;
-				}
-				$ele.html(str.substring(0, progress) + (progress & 1 ? '_' : ''));
-				if (progress >= str.length) {
-					clearInterval(timer);
-				}
-			}, 75);
-		});
-		return this;
-	};
-})(jQuery);
 
 function timeElapse(date){
 	var current = Date();
@@ -110,27 +83,20 @@ function timeElapse(date){
 	if (seconds < 10) {
 		seconds = seconds;
 	}
-	var result = "<span class=\"digit\">" + days + "</span> days <span class=\"digit\">" + hours + "</span> hours <span class=\"digit\">" + minutes + "</span> minutes <span class=\"digit\">" + seconds + "</span> seconds"; 
+	var result = "<span class=\"digit\">" + days + "</span> days <span class=\"digit\">" + hours + "</span> hours <span class=\"digit\">" + minutes + "</span> minutes <span class=\"digit\">" + seconds + "</span> seconds";
 	$("#elapseClock").html(result);
 }
 
 function showMessages() {
-	adjustWordsPosition();
 	$('#messages').fadeIn(5000, function() {
-		showLoveU();
+		showWords();
 	});
-}
-
-function adjustWordsPosition() {
-	$('#words').css("position", "absolute");
-	$('#words').css("top", $("#garden").position().top + 195);
-	$('#words').css("left", $("#garden").position().left + 70);
 }
 
 function adjustCodePosition() {
 	$('#code').css("margin-top", ($("#garden").height() - $("#code").height()) / 2-80);
 }
 
-function showLoveU() {
-	$('#loveu').fadeIn(3000);
+function showWords() {
+	$('#newyers').fadeIn(1000);
 }
